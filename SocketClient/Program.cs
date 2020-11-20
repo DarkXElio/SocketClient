d1d65ca75dab4b71e70faf997d6df3ce438a7be5
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-20 11:18:48 +0100

    Iniziamo

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -6,7 +6,7 @@ namespace SocketClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Socket Client server");
         }
     }
 }
