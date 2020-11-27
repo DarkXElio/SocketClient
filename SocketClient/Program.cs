cfefe528dc9fca895fa0e1905ab575867371242f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-27 12:30:46 +0100

    Fatto

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -2,65 +2,77 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-
 namespace SocketClient
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-            //Creare il mio socketlistener
-            //1) specifico che versione IP
-            //2) tipo di socket. Stream.
-            //3) protocollo a livello di trasporto
-            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            string strIPAdress = "192.168.5.198", strPort = "";
-            
-            IPAddress ipaddr = null;
-            int nPort;
+        {
+            // Dichiarazione Socket Client
+            Socket client = null;
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // Dichiarazione EndPoint del server
+            IPAddress ipAddr = null;
+            string strIPAddress = "";
+            string strPort = "";
+            int nPort = 0;
 
 
+            //Dichiarazione Variabili per comunicare con il server
+            string sendString = "";
+            string receivedString = "";
+            byte[] sendBuff = new byte[128];
+            byte[] recvBuff = new byte[128];
+            int nReceivedBytes = 0;
             try
             {
-                
-                Console.WriteLine("IP del Server: ");
-                strIPAdress = Console.ReadLine();
-                Console.WriteLine("Port del Server: ");
+                // Settagio da Console dell'EndPoint
+                Console.WriteLine("Benvenuto nel Client Socket");
+                Console.Write("IP del Server: ");
+                strIPAddress = Console.ReadLine();
+                Console.Write("Porta del Server: ");
                 strPort = Console.ReadLine();
-                if(!IPAddress.TryParse(strIPAdress.Trim(),out ipaddr))
-                {
-                    Console.WriteLine("IP non valido.");
-                    return;
-                }
-                if(!int.TryParse(strPort,out nPort))
-                {
-                    Console.WriteLine("Porta non valido.");
-                    return;
-                }
-                if(nPort<=0 || nPort >= 65535)
-                {
-                    Console.WriteLine("Porta non valido.");
-                    return;
-                }
-                Console.WriteLine("Endpoint del server " + ipaddr.ToString() + ":" + nPort);
-                client.Connect(ipaddr, nPort);
 
-                byte[] buff = new byte[128];
-                string sendString = "";
-                string ReciveString = "";
-                int recivedButes = 0;
+                if (!IPAddress.TryParse(strIPAddress.Trim(), out ipAddr))
+                {
+                    Console.Write("IP non valido.");
+                    return;
+                }
+                if (!int.TryParse(strPort, out nPort))
+                {
+                    Console.Write("Porta non valida.");
+                    return;
+                }
+                if (nPort <= 0 || nPort >= 65535)
+                {
+                    Console.Write("Porta non valida.");
+                    return;
+                }
+                Console.WriteLine("Endpoint: " + ipAddr.ToString() + " " + nPort);
+
+                // Connessione al server
+                client.Connect(ipAddr, nPort);
+
+                //Inizio chat con il server
+                Console.WriteLine("Chatta con il server. ");
                 while (true)
                 {
-                    Console.WriteLine("Manda un messaggio");
+                    // Prendo il messaggio & condizione di uscita
                     sendString = Console.ReadLine();
-                    Encoding.ASCII.GetBytes(sendString).CopyTo(buff,0);
-                    client.Send(buff);
+                    //dico al server di interrompersi
+                    sendBuff = Encoding.ASCII.GetBytes(sendString);
+                    client.Send(sendBuff);
 
-                    Array.Clear(buff, 0, buff.Length);
-                    recivedButes = client.Receive(buff);
-                    ReciveString = Encoding.ASCII.GetString(buff, 0, recivedButes);
-                    Console.WriteLine(ReciveString);
+                    if (sendString.ToUpper().Trim() == "QUIT")
+                    {
+
+                        break;
+                    }
+                    //Pulisco il buffer e ricevo il messaggio
+                    Array.Clear(recvBuff, 0, recvBuff.Length);
+                    nReceivedBytes = client.Receive(recvBuff);
+                    receivedString = Encoding.ASCII.GetString(recvBuff);
+                    Console.WriteLine("S: " + receivedString);
                 }
 
 
@@ -68,9 +80,22 @@ namespace SocketClient
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-               
             }
-            
+            finally
+            {
+                /* In ogni occasione chiudo la connessione per sicurezza */
+                if (client != null)
+                {
+                    if (client.Connected)
+                    {
+                        client.Shutdown(SocketShutdown.Both);//disabilita la send e receive
+                    }
+                    client.Close();
+                    client.Dispose();
+                }
+            }
+            Console.WriteLine("Premi Enter per chiudere...");
+            Console.ReadLine();
         }
     }
 }
